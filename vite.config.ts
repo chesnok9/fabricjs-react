cd27dfe32bda320cd9d0e9293a80c7fb451df05d
chesnok9 <pavel.chesnovsky@gmail.com>
2023-03-27 21:42:57 +0300

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     dts({
       insertTypesEntry: true
-    })
+    }),
+    react()
   ],
   build: {
     lib: {
